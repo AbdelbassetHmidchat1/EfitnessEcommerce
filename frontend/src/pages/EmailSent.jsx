9ff1dfb590851a89ab89fcf20628377a7518761c
Abdelbasset <abdelbassethmidchat1@gmail.com>
2024-11-07 09:05:45 +0100

    Update EmailSent.jsx

--- frontend/src/pages/EmailSent.jsx
+++ frontend/src/pages/EmailSent.jsx
@@ -2,7 +2,7 @@ export default function EmailSent() {
   return (
     <>
       <div className="w-screen grid place-items-center h-screen bg-black">
-        <p>A verification link has been sent to your email</p>
+        <p>A verification link has been sent to your email (if not sent please check your spam)</p>
       </div>
     </>
   );
